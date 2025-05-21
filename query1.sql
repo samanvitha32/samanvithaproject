dd620de31fb9c8a630fd9c6f74d971d2c5d535eb
Samanvitha <samanvitha@example.com>
2025-05-21 09:56:15 -0500

    sql

---
+++ query1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Department VARCHAR(50),
+    HireDate DATE
+);
